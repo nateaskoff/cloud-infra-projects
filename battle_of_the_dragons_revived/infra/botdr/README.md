0a17d645397d035914e31cbdb069035a2ee9ffcd
Nate Askoff <nate.askoff@outlook.com>
2024-11-10 14:29:57 -0500

    start building BOTDR

--- battle_of_the_dragons_revived/infra/botdr/README.md
+++ battle_of_the_dragons_revived/infra/botdr/README.md
@@ -24,6 +24,9 @@ No modules.
 |------|------|
 | [aws_acm_certificate.web_cert](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/acm_certificate) | resource |
 | [aws_acm_certificate_validation.web_cert_val](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/acm_certificate_validation) | resource |
+| [aws_cloudfront_distribution.cf_dist_botdr_web](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/cloudfront_distribution) | resource |
+| [aws_cloudfront_origin_access_control.default](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/cloudfront_origin_access_control) | resource |
+| [aws_cloudfront_response_headers_policy.default](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/cloudfront_response_headers_policy) | resource |
 | [aws_route53_record.botdr_app](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/route53_record) | resource |
 | [aws_route53_record.botdr_web](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/resources/route53_record) | resource |
 | [aws_route53_zone.botdr_zone](https://registry.terraform.io/providers/hashicorp/aws/5.75.0/docs/data-sources/route53_zone) | data source |
---
+++ battle_of_the_dragons_revived/infra/botdr/cloudfront.tf
@@ -0,0 +1,104 @@
+resource "aws_cloudfront_distribution" "cf_dist_botdr_web" {
+  #checkov:skip=CKV2_AWS_47
+  #checkov:skip=CKV_AWS_68
+  #checkov:skip=CKV_AWS_310
+  #checkov:skip=CKV_AWS_86
+  origin {
+    domain_name            = aws_s3_bucket.botdr_web.bucket_regional_domain_name
+    origin_access_identity = aws_cloudfront_origin_access_identity.cf_oai_botdr_web.cloudfront_access_identity_path
+    origin_id              = "${var.env}-botdr-web-s3-oid"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  comment             = "${var.env}-botdr-web-cf-dist"
+  default_root_object = "index.html"
+
+  aliases = var.env == "dev" ? ["web${var.env}.battleofthedragonsrevived.com"] : ["battleofthedragonsrevived.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.env}-botdr-web-s3-oid"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 300
+    max_ttl                = 3600
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations = [
+        "US",
+        "CA"
+      ]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.web_cert.arn
+    ssl_support_method             = "sni-only"
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "${lower(var.env)}-botdr-cf-pri-site-oac"
+  description                       = "default cf origin access control"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_response_headers_policy" "default" {
+  name    = "${lower(var.env)}-swgs-cf-pri-site-resp-hdrs-pol"
+  comment = "Cloudfront response headers policy for environment ${var.env}"
+
+  security_headers_config {
+    strict_transport_security {
+      access_control_max_age_sec = 31536000
+      include_subdomains         = true
+      override                   = true
+      preload                    = true
+    }
+  }
+
+  cors_config {
+    access_control_allow_credentials = true
+
+    access_control_allow_headers {
+      items = [
+        "X-Custom-Header",
+        "Upgrade-Insecure-Requests"
+      ]
+    }
+
+    access_control_allow_methods {
+      items = [
+        "GET",
+        "POST"
+      ]
+    }
+
+    access_control_allow_origins {
+      items = [
+        "*"
+      ]
+    }
+
+    origin_override = true
+  }
+}
