87aba39348ab26922643c184e959d1636e3309ac
Nate Askoff <nate.askoff@outlook.com>
2024-11-11 17:23:02 -0500

    add mod bkt and user

--- battle_of_the_dragons_revived/infra/botdr/iam.tf
+++ battle_of_the_dragons_revived/infra/botdr/iam.tf
@@ -7,5 +7,5 @@ resource "aws_iam_user_policy" "botdr_fly_io_user_policy" {
   name = "${var.env}-botdr-fly-io-user-policy"
   user = aws_iam_user.botdr_fly_io_user.name
 
-  policy = jsonencode(data.aws_iam_policy_document.botdr_fly_io_user_policy_doc.json)
+  policy = data.aws_iam_policy_document.botdr_fly_io_user_policy_doc.json
 }
