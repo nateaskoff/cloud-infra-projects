59720d1b6137df55796255e242cf3533a4e4cbea
Nate Askoff <nate.askoff@outlook.com>
2024-11-10 19:30:42 -0500

    use dev environment for deploy

--- .ci_cd/scripts/tf_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if terraform init -input=false; then
-    echo "Terraform init successful";
-else
-    echo "Creating workspace";
-    terraform workspace new $TF_WORKSPACE;
-    terraform init -input=false;
-fi
