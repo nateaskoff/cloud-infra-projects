efb088b7912bd563b89df7c3816814837486a65d
Nate Askoff <nate.askoff@outlook.com>
2024-11-13 16:03:09 -0500

    start working on ai resume project

--- ai_resume/infra/main.tf
+++ ai_resume/infra/main.tf
@@ -1,12 +1,7 @@
 terraform {
   required_version = "1.9.8"
 
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.state_key
-  }
+  backend "azurerm" {}
 
   required_providers {
     azurerm = {
