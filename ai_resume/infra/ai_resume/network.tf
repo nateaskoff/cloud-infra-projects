ec7057e8f4ad3bb7ce1868effcf4aab061703c91
Nate Askoff <nate.askoff@outlook.com>
2024-11-14 17:07:30 -0500

    begin building ai resume

--- ai_resume/infra/ai_resume/network.tf
+++ ai_resume/infra/ai_resume/network.tf
@@ -19,3 +19,14 @@ resource "azurerm_subnet" "subnet" {
     "10.0.1.0/24"
   ]
 }
+
+resource "azurerm_network_security_group" "vnet_sec_group" {
+  name                = "${var.env}-vnet-sec-group-ai-resume"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.vnet_sec_group.id
+}
