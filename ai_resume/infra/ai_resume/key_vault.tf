0a382b45222840c642ccbda816816d4bebd620a4
Nate Askoff <nate.askoff@outlook.com>
2024-11-14 16:14:18 -0500

    begin building ai resume

--- ai_resume/infra/ai_resume/key_vault.tf
+++ ai_resume/infra/ai_resume/key_vault.tf
@@ -9,7 +9,7 @@ resource "azurerm_key_vault" "key_vault" {
 
   sku_name = "standard"
 
-  access_policy = {
+  access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
